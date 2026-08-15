656715c4abbf71b92e25cf8f6acfe7abd0810cdd
James Gaier <james.gaier003@gmail.com>
2026-08-15 13:14:20 -0700

    Add SIGINT handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use server::handle_connection;
 use std::io;
 use tokio::net::TcpListener;
+use tokio::sync::oneshot;
 
 // Ip and port data
 #[derive(Parser)]
@@ -17,9 +18,10 @@ struct Cli {
 // TODO: Add unit tests for the server module
 #[tokio::main]
 async fn main() -> io::Result<()> {
+
     let cli = Cli::parse();
 
-    let mut server_host = String::from("localhost");
+    let mut server_host = String::from("127.0.0.1");
     if let Some(host) = cli.host {
         // use host validator to make sure host is valid
         if hostname_validator::is_valid(&host) {
@@ -38,14 +40,35 @@ async fn main() -> io::Result<()> {
     let listener = TcpListener::bind(&url).await?;
     println!("Listening on {url}");
 
-    loop {
-        // if you can accept a connection accept it
-        if let Ok((socket, addr)) = listener.accept().await {
-            println!("Accepted connection from {addr}");
-            // pass the task off to another thread
-            tokio::spawn(async move {
-                handle_connection(socket).await;
-            });
-        }
+
+    // create a oneshot channel... we are only ever gunna hit CTRL+C once
+    let (resp_tx, resp_rx) = oneshot::channel();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        resp_tx.send(true).unwrap();
+    });
+
+    // do whichever is not blocking first
+    // i.e. if we do not get a message saying CTRL+C was pressed we can have a server running
+    // the first arm is a channel which will block until a message is received
+    // the second arm is a future which will block forever
+    tokio::select! {
+        val = resp_rx => {
+            println!("CTRL+C pressed!");
+        },
+        _ = async {
+            loop {
+                // if you can accept a connection accept it
+                if let Ok((socket, addr)) = listener.accept().await {
+                    println!("Accepted connection from {addr}");
+                    // pass the task off to another thread
+                    tokio::spawn(async move {
+                        handle_connection(socket).await;
+                    });
+                }
+            }
+        } => {}
     }
+
+    Ok(())
 }
